ed19b63fd9658d14338e03aeccdc86d15470245f
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 08:27:34 -0300

    Create desafio10 - reais em dólares.py

---
+++ desafio10 - reais em dólares.py
@@ -0,0 +1,2 @@
+valor_reais = float(input('Quanto de dinheiro em Reais tenho em carteira? '))
+print('Você pode comprar US$ {:.2f} dolares, com R$ {} reais!'.format((valor_reais / 3.27),valor_reais))
