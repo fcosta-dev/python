7cfcca26a52e5b89ef7cd267bd49b5f4094c6f6c
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 08:20:40 -0300

    Create desafio05 - antecessor e sucessor de um número.py

---
+++ desafio05 - antecessor e sucessor de um número.py
@@ -0,0 +1,9 @@
+print('====== DESAFIO 05 ======')
+n1 = int(input('Digite um número: '))
+print('O número digitado foi: {} !'.format(n1))
+print('Seu antecessor é: {}'.format(n1-1))
+print('Seu sucessor é: {}'.format(n1+1))
+print('Tenha um bom dia!')
+
+
+
