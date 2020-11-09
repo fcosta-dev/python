f2feb0e966c9c23673df0ee41d790ec714783008
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 22:24:10 -0300

    Delete desafio11 - largura e altura, uso de tinta.py

--- desafio11 - largura e altura, uso de tinta.py
+++
@@ -1,3 +0,0 @@
-largura = float(input('Digite a largura da parede (em metros): '))
-altura = float(input('Digite a altura da parede (em metros): '))
-print('Sera necessário a quantidade de {:.2f} litros de tinta para pintar essa área !'.format((largura*altura)/2))
