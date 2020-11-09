e4fc1f9534b7039c6b3b081f4d0443d473c1781d
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 23:42:50 -0300

    Create 04. strings.py

---
+++ Dankicode - Do Basico ao Avançado/04. strings.py
@@ -0,0 +1,29 @@
+print("ola mundo")
+print('ola mundo')
+
+w = "Curso de Python3"
+print(w)
+
+"""comentário"""
+
+a = """ola,
+este eh o curso de python
+espero que goste"""
+print(a)
+
+
+#metodos das Strings
+b = " Ola mundo "
+print(b)
+print(b.strip())
+
+c = "Ola Mundo"
+print(c.lower())
+
+d = "Ola Mundo"
+print(d.upper())
+
+e = "Ola"
+f = "Mundo"
+g = e + " " + f
+print(g)
