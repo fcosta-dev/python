f8e869c47fcb182d7706a0914cbf0aab66d3b0bb
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 23:42:46 -0300

    Create 03. variaveis.py

---
+++ Dankicode - Do Basico ao Avançado/03. variaveis.py
@@ -0,0 +1,27 @@
+x = 5 # x recebe o valor 5
+y = 3.5
+z = 1+2j
+
+w = "Ana"
+w = 'Ana'
+
+# A-z, 0-9 e _
+#altura, Altura e ALTURA são variaveis diferentes
+
+h, j, l = 1, 3, 5
+print(h,j,l)
+
+a = b = c = " faz curso de Python3"
+
+print(a)
+print(b)
+print(c)
+
+h = w + a
+print(h)
+
+l = x + j
+print(l)
+
+l = w + x
+print(l)
