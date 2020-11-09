1f1d095ab39fd45fe0e743c8dd15e2861c1d2899
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 22:28:11 -0300

    Create desafio71a - simulador de caixa eletrônico.py

---
+++ Mundo 02 - Básico - Estruturas de repetição/desafio71a - simulador de caixa eletrônico.py
@@ -0,0 +1,26 @@
+print('*'*60)
+print('{:^30}'.format('Banco Costa'))
+print('*'*60)
+
+valor = int(input('Digite o valor a ser sacado: R$ '))
+total = valor
+céd = 50
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO COSTA! Tenha um bom dia!')
