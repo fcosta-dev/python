eba50e34f3abe0c1ef7ddb69dfd4ad88482dd262
Fernando G. Costa <fgosuendacosta@gmail.com>
2020-11-08 22:24:33 -0300

    Delete desafio20 - embaralhar nomes da lista com shuffle.py

--- desafio20 - embaralhar nomes da lista com shuffle.py
+++
@@ -1,9 +0,0 @@
-import random
-aluno1 = str(input('Digite o primeiro nome: '))
-aluno2 = str(input('Digite o segundo nome: '))
-aluno3 = str(input('Digite o terceiro nome: '))
-aluno4 = str(input('Digite o quarto nome: '))
-lista = [aluno1, aluno2, aluno3, aluno4]
-random.shuffle(lista)
-print('A ordem de apresentação será: ')
-print(lista)
